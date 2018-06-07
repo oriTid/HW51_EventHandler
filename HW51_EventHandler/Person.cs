1db9cb9270dc6fbb9814bd237acbafed12e45d65
oriTid <ori@adidance.co.il>
2018-06-07 16:21:23 +0300

    updated

--- HW51_EventHandler/Person.cs
+++ HW51_EventHandler/Person.cs
@@ -1,22 +1,23 @@
 ﻿namespace HW51_EventHandler
 {
+    //יצירת דלגייט 
     delegate string SigningDelegate(string fullName);
      class Person
 
     {
-
-        public event SigningDelegate SigningEvent;
+       
+        public event SigningDelegate SigningEvent;  //יצירת ארוע מסוג הדלגייט 
         public string FullName { get; set; }
         public bool WantToSign { get; set; }
 
 
-        public string SignAction()
+        public string SignAction()   //יצירת פונקציה שבמידה ועומדת בכל התנאים מפעילה את הארוע מסוג הדלגייט 
         {
             if (WantToSign)
             {
                 if (SigningEvent != null)
                 {
-                    return SigningEvent(FullName);
+                    return SigningEvent(FullName);  //הפעלת הארוע 
                 }
             }
             return "";
--- HW51_EventHandler/Program.cs
+++ HW51_EventHandler/Program.cs
@@ -10,40 +10,42 @@ namespace HW51_EventHandler
             List<Person> peopleList = new List<Person>();
             List<Signature> signatureList = new List<Signature>();
 
-            peopleList.Add(GetPerson("Bob Arnold", true));
-            peopleList.Add(GetPerson("Alice Avery", true));
-            peopleList.Add(GetPerson("Albert Bailey", true));
-            peopleList.Add(GetPerson("Matt Baker", true));
-            peopleList.Add(GetPerson("Carol Campbell", true));
-            peopleList.Add(GetPerson("Kevin Brown", true));
-            peopleList.Add(GetPerson("Lisa Bower", true));
+            //הוספת אנשים לרשימת האנשים, שכוללת את השם המלא והאם הוא מוכן לחתום
+            peopleList.Add(GetNewPerson("Bob Arnold", true));
+            peopleList.Add(GetNewPerson("Alice Avery", true));
+            peopleList.Add(GetNewPerson("Albert Bailey", true));
+            peopleList.Add(GetNewPerson("Matt Baker", true));
+            peopleList.Add(GetNewPerson("Carol Campbell", true));
+            peopleList.Add(GetNewPerson("Kevin Brown", true));
+            peopleList.Add(GetNewPerson("Lisa Bower", true));
 
+            //רישום לארוע של החתימה, והגדרת היישום שלו
             peopleList[0].SigningEvent += (x) => x.ToLower();
             peopleList[1].SigningEvent += (x) => x.Replace(" ", "");
             peopleList[2].SigningEvent += (x) => x[0].ToString() + x[x.IndexOf(" ") + 1];
-            peopleList[3].SigningEvent += (x) => x[0].ToString() + x.Substring(x.IndexOf(" ")+1);
+            peopleList[3].SigningEvent += (x) => x[0].ToString() + x.Substring(x.IndexOf(" ") + 1);
             peopleList[4].SigningEvent += (x) => x.ToUpper();
-            peopleList[5].SigningEvent += (x) => x.Split(' ')[1]+"."+ x.Split(' ')[0];
-            peopleList[6].SigningEvent += (x) => x.Substring(0,x.IndexOf(" ")).ToLower();
-                        
+            peopleList[5].SigningEvent += (x) => x.Split(' ')[1] + "." + x.Split(' ')[0];
+            peopleList[6].SigningEvent += (x) => x.Substring(0, x.IndexOf(" ")).ToLower();
+
+            //ריצה בלולאה שמוסיפה לרשימת החתימות את השם  וכמו כן מפעילה פונקציה 
             foreach (Person item in peopleList)
             {
                 signatureList.Add(new Signature(item.FullName, item.SignAction()));
             }
-            
 
-
+            //ריצה בלולאה להצגת החתימות בקונסול 
             foreach (Signature sign in signatureList)
             {
                 Console.WriteLine("Full name: " + sign.FullName + " signature: " + sign.FullSignature);
-                
+
             }
 
             Console.ReadLine();
         }
 
-
-        private static Person GetPerson(string name, bool wantsToSign)
+        //פונקציה שיוצרת פרסון חדש. מקבלת שם והאם מוכן לחתום, ומחזירה אוביקט מאותחל מסוג פרסון 
+        private static Person GetNewPerson(string name, bool wantsToSign)
         {
             Person p = new Person();
             p.FullName = name;
